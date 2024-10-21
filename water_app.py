39e1dc757a1dabcee577c1c17399883e6d4c5097
vitormalagutti <vitoramalagutti@gmail.com>
2024-10-21 10:47:33 +0200

    Update water_app.py

--- water_app.py
+++ water_app.py
@@ -143,7 +143,7 @@ if uploaded_file:
         lat="Y",
         lon="X",
         z=None,  # You can use 'Population' or other intensity columns if needed
-        radius=10,
+        radius=7,
         center=dict(lat=gdf['Y'].mean(), lon=gdf['X'].mean()),
         zoom=map_zoom,
         mapbox_style="carto-positron",
