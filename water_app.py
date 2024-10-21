bd03c9b09447da101bbdcdba23747def1d930a5e
vitormalagutti <vitoramalagutti@gmail.com>
2024-10-21 16:23:52 +0200

    Update water_app.py

--- water_app.py
+++ water_app.py
@@ -288,10 +288,10 @@ if uploaded_file:
             st.markdown("### 📈 Monthly Water Consumption Distribution (l/p/d)")
 
             fig, ax = plt.subplots(figsize=(8,4))
-            ax.plot(df_factors['Month'], df_factors['Monthly Daily Consumption - l/p/d'], marker='o', color='b')
+            ax.plot(df_factors['Month'], df_factors['Monthly Daily Consumption - l/p/d'], marker='o', color='lightblue')
             ax.set_ylabel('Monthly Water Consumption (l/p/d)')
             ax.set_title('Monthly Water Consumption Distribution')
-            ax.grid(True)
+            ax.grid(True, linestyle ='-')
 
             # Apply the if condition for y-axis limits
             if avg_litres_per_person < 200:
