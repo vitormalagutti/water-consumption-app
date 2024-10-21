3e6f9cce26eff250ca700754715dcd35f5655d35
vitormalagutti <vitoramalagutti@gmail.com>
2024-10-21 17:24:42 +0200

    Update water_app.py

--- water_app.py
+++ water_app.py
@@ -241,9 +241,9 @@ if uploaded_file:
                                         "category": "Uber",
                                         "colors": ["#5A1846", "#900C3F", "#C70039", "#FF5733", "#FFC300"]
                                     }
-                                }
-                            },
+                                },
                         "isVisible" : True
+                            }
                         }
                     ]
                 }
