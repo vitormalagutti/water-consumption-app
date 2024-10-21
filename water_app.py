6f386c07207a325dad07f9a150cafeaeafa48a05
vitormalagutti <vitoramalagutti@gmail.com>
2024-10-21 17:21:36 +0200

    Update water_app.py

--- water_app.py
+++ water_app.py
@@ -340,28 +340,28 @@ if uploaded_file:
             # Create heatmaps based on selection
             if heatmap_type == "All Buildings":
                 st.markdown("#### 🔥 Heatmap of All Building Locations")
-                kepler_map = KeplerGl(height=800, config=config_grid)
+                kepler_map = KeplerGl(height=800, config=config_heatmap)
                 kepler_map.add_data(data=gdf, name="Water Consumption Data")
                 keplergl_static(kepler_map)
 
             elif heatmap_type == "Illegal Connections":
                 st.markdown("#### 🔥 Heatmap of Illegal Connections")
                 gdf_illegal = gdf[gdf['User Type'] == 'Illegal'] 
-                kepler_map = KeplerGl(height=800, config=config_grid)
+                kepler_map = KeplerGl(height=800, config=config_heatmap)
                 kepler_map.add_data(data=gdf_illegal, name="Water Consumption Data")
                 keplergl_static(kepler_map)
 
             elif heatmap_type == "Legal Connections":
                 st.markdown("#### 🔥 Heatmap of Legal Connections")
                 gdf_legal = gdf[gdf['User Type'] == 'Legal']
-                kepler_map = KeplerGl(height=800, config=config_grid)
+                kepler_map = KeplerGl(height=800, config=config_heatmap)
                 kepler_map.add_data(data=gdf_legal, name="Water Consumption Data")
                 keplergl_static(kepler_map)
                 
             elif heatmap_type == "Non-Users":
                 st.markdown("#### 🔥 Heatmap of Non-Users")
                 gdf_non_user = gdf[gdf['User Type'] == 'Non-user']
-                kepler_map = KeplerGl(height=800, config=config_grid)
+                kepler_map = KeplerGl(height=800, config=config_heatmap)
                 kepler_map.add_data(data=gdf_non_user, name="Water Consumption Data")
                 keplergl_static(kepler_map)
    
