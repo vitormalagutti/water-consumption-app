f9e20d418b34b2dce7d77f6a57447169f1534412
vitormalagutti <vitoramalagutti@gmail.com>
2024-10-21 14:15:11 +0200

    Update water_app.py

--- water_app.py
+++ water_app.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 import geopandas as gpd
 import folium
 import pydeck as pdk
+import json
 from keplergl import KeplerGl
 from shapely.geometry import Point
 from streamlit_folium import folium_static
