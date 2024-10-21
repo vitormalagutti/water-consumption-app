6a55eed9d6126759172ac8ebbe3a5ab30934ec6d
vitormalagutti <vitoramalagutti@gmail.com>
2024-10-21 14:32:54 +0200

    Update water_app.py

--- water_app.py
+++ water_app.py
@@ -126,7 +126,7 @@ if uploaded_file:
 
         # Create a dynamic configuration for KeplerGL
         # Define a valid dynamic configuration for KeplerGL
-        config1 = {
+       # config1 = {
             'version': 'v1',
             'config': {
             'mapState': {
@@ -136,9 +136,9 @@ if uploaded_file:
             }}}
         
 
-        kepler_map = KeplerGl(height=600, config=config1)
-        kepler_map.add_data(data=df, name="Water Consumption Data")
-        kepler_map.save_to_html(file_name="kepler_map_dynamic.html")
+#        kepler_map = KeplerGl(height=600, config=config1)
+#        kepler_map.add_data(data=df, name="Water Consumption Data")
+ #       kepler_map.save_to_html(file_name="kepler_map_dynamic.html")
     
 
         # Display the saved KeplerGL map in Streamlit
