e986baf9ea2c523156c83bd74e42866fe4e361c9
vitormalagutti <vitoramalagutti@gmail.com>
2024-10-22 17:18:00 +0200

    Update water_app.py

--- water_app.py
+++ water_app.py
@@ -377,7 +377,7 @@ if uploaded_file:
 
         with col1:
             st.markdown("### 💧 Water Consumption per Zone (Monthly)")
-            st.dataframe(water_demand_dma)
+            st.dataframe(water_demand_dma.transpose())
 
         with col2:
             st.markdown("### 📉 Monthly Water Consumption Variation by Zone")
