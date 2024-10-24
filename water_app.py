e47dd6402935f9472342d4d42caeb2542e7c6b88
vitormalagutti <vitoramalagutti@gmail.com>
2024-10-24 17:15:14 +0200

    Update water_app.py

--- water_app.py
+++ water_app.py
@@ -595,6 +595,12 @@ with tab1:
             else:
                 st.markdown("## Please, upload a volume's file")
 
+            if volume_file:
+                    processed_df = process_volume_or_value_file(value_file)
+                    if processed_df is not None:
+                        st.dataframe(processed_df)  # Display the cleaned data
+            else:
+                    st.markdown("## Please, upload a value's file")   
 
         with tab6:
             st.markdown("### 🗺️ Interactive Maps with Google Satellite Basemap")
