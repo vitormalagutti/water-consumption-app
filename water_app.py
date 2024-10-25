5d4a9b22c7b4ea6d13efdbdce892f68fd5cf6e65
vitormalagutti <vitoramalagutti@gmail.com>
2024-10-25 17:58:41 +0200

    Update water_app.py

--- water_app.py
+++ water_app.py
@@ -811,8 +811,8 @@ with tab1:
 
 
                 # Call the function for both zone and DMA merged dataframes
-                plot_combined_demand_billed(zone_merged_df,unique_zones, title="Zone Demand vs Billed Volumes with % Billed")
-                plot_combined_demand_billed(dma_merged_df,unique_dmas, title="DMA Demand vs Billed Volumes with % Billed")
+                simple_plot_demand_billed(zone_merged_df,unique_zones, title="Zone Demand vs Billed Volumes with % Billed")
+                simple_plot_demand_billed(dma_merged_df,unique_dmas, title="DMA Demand vs Billed Volumes with % Billed")
 
 
 
