5468807345b4f68322a6dc78c20fd8c486de2d04
vitormalagutti <vitoramalagutti@gmail.com>
2024-10-25 10:58:00 +0200

    Update water_app.py

--- water_app.py
+++ water_app.py
@@ -633,6 +633,7 @@ with tab1:
                 if 'Zone' in merged_df.columns:
                     zone_volume_df = pd.merge(merged_df[['Block Number', 'Zone']], volume_summed, on='Block Number', how='left')
                     zone_volume_df = zone_volume_df.groupby('Zone').sum(numeric_only=True).reset_index().drop(columns=["Block Number"])
+                    zone_volume_df = zone_volume_df.round(0).astype(int)
                     st.markdown("### Zone Billed Data Summed by Volume")
                     st.dataframe(zone_volume_df)
 
@@ -640,6 +641,7 @@ with tab1:
                 if 'Zone' in merged_df.columns:
                     zone_value_df = pd.merge(merged_df[['Block Number', 'Zone']], value_summed, on='Block Number', how='left')
                     zone_value_df = zone_value_df.groupby('Zone').sum(numeric_only=True).reset_index().drop(columns=["Block Number"])
+                    zone_value_df = zone_value_df.round(0).astype(int) 
                     st.markdown("### Zone Billed Data Summed by Value")
                     st.dataframe(zone_value_df)
 
@@ -647,6 +649,7 @@ with tab1:
                 if 'DMA' in merged_df.columns:
                     dma_volume_df = pd.merge(merged_df[['Block Number', 'DMA']], volume_summed, on='Block Number', how='left')
                     dma_volume_df = dma_volume_df.groupby('DMA').sum(numeric_only=True).reset_index().drop(columns=["Block Number"])
+                    dma_volume_df = dma_volume_df.round(0).astype(int)
                     st.markdown("### DMA Billed Data Summed by Volume")
                     st.dataframe(dma_volume_df)
 
@@ -654,6 +657,7 @@ with tab1:
                 if 'DMA' in merged_df.columns:
                     dma_value_df = pd.merge(merged_df[['Block Number', 'DMA']], value_summed, on='Block Number', how='left')
                     dma_value_df = dma_value_df.groupby('DMA').sum(numeric_only=True).reset_index().drop(columns=["Block Number"])
+                    dma_value_df = dma_value_df.round(0).astype(int)
                     st.markdown("### DMA Billed Data Summed by Value")
                     st.dataframe(dma_value_df)
 
