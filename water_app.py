e695250736cab7c7f17515e7583dde3bf025dfa5
vitormalagutti <vitoramalagutti@gmail.com>
2024-10-28 14:38:37 +0100

    Update water_app.py

--- water_app.py
+++ water_app.py
@@ -484,10 +484,38 @@ with tab1:
         else:
             st.sidebar.write("No data available for Zone or DMA visualization.")
 
-        # Sidebar multiselect for filtering DMAs/Zones
-        # Extract unique DMA values from the filtered DataFrame and convert to integers
-        available_dmas_zones = [int(value) for value in filtered_df[visualization_type].unique() if not pd.isna(value)]
-        selected_dmas_zones = st.sidebar.multiselect("Select DMAs/Zones to Display", available_dmas_zones, default=available_dmas_zones)
+        if volume_file and value_file and correlation_file and buildings_file:
+            # Sidebar multiselect for filtering DMAs/Zones
+            # Extract unique DMA values from the filtered DataFrame and convert to integers
+            available_dmas_zones = [int(value) for value in filtered_df[visualization_type].unique() if not pd.isna(value)]
+            selected_dmas_zones = st.sidebar.multiselect("Select DMAs/Zones to Display", available_dmas_zones, default=available_dmas_zones)
+
+            # Process each file
+            volume_df = process_volume_or_value_file(volume_file)
+            value_df = process_volume_or_value_file(value_file)
+            correlation_df = process_block_subscription_file(correlation_file)
+
+            # Step 2: Join the correlation file with the original buildings file on 'Block Number'
+            merged_df = pd.merge(buildings_df, correlation_df, on='Block Number', how='left')
+
+            # Step 3: Merge volume_df and value_df on 'Subscription Number'
+            volume_df = pd.merge(correlation_df, volume_df, on='Subscription Number', how='left')
+            value_df = pd.merge(correlation_df, value_df, on='Subscription Number', how='left')
+
+            # Step 4: Group by 'Block Number' and sum Volume and Value
+            volume_summed = volume_df.groupby('Block Number').sum(numeric_only=True).reset_index()
+            value_summed = value_df.groupby('Block Number').sum(numeric_only=True).reset_index()
+
+            # Step 5: Merge the summed volumes and values back to the original df
+            billed_df = pd.merge(merged_df, volume_summed, on='Block Number', how='left')
+            billed_df = pd.merge(billed_df, value_summed, on='Block Number', how='left', suffixes=('_volume', '_value'))
+
+            # Drop unnecessary columns
+            columns_to_drop = ['Population', 'Status', 'Subscription Number_x', 'Subscription Number_y', "Subscription Number"]
+            billed_df = billed_df.drop(columns=columns_to_drop, errors='ignore')
+
+        
+
 
 
         # Sidebar input to select analysis type
