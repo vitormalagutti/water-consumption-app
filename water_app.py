971f009996b460657f2d1e19d7edc46d4b4f268d
vitormalagutti <vitoramalagutti@gmail.com>
2024-10-28 16:53:08 +0100

    Update water_app.py

--- water_app.py
+++ water_app.py
@@ -897,7 +897,7 @@ with tab1:
 
                 # Step 2: Join the correlation file with the original buildings file on 'Block Number'
                 merged_df = pd.merge(buildings_df, correlation_df, on='Block Number', how='left')
-
+                volume_df
                 # Step 3: Merge volume_df and value_df on 'Subscription Number'
                 volume_df = pd.merge(correlation_df, volume_df, on='Subscription Number', how='left')
                 value_df = pd.merge(correlation_df, value_df, on='Subscription Number', how='left')
@@ -906,6 +906,8 @@ with tab1:
                 volume_summed = volume_df.groupby('Block Number').sum(numeric_only=True).reset_index()
                 value_summed = value_df.groupby('Block Number').sum(numeric_only=True).reset_index()
                 
+                volume_df
+                volume_summed
                 # Step 5: Merge the summed volumes and values back to the original df
                 billed_df = pd.merge(merged_df, volume_summed, on='Block Number', how='left')
                 billed_df = pd.merge(billed_df, value_summed, on='Block Number', how='left', suffixes=('_volume', '_value'))
