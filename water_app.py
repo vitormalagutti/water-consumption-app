af8f09fcb9b2c2be0dccc2f2859178481ae0188b
vitormalagutti <vitoramalagutti@gmail.com>
2024-10-28 14:59:03 +0100

    Update water_app.py

--- water_app.py
+++ water_app.py
@@ -336,6 +336,7 @@ def plot_billed_vs_expected(df, n, selected_dmas_zones, start_date_dt, end_date_
     billed_columns = [col for col in df.columns[:n] if col.split(' ')[-1] in selected_dmas_zones]
     expected_columns = [col for col in df.columns[n:2*n] if col.split(' ')[-1] in selected_dmas_zones]
 
+    start_date_dt
     # Filter DataFrame to only include selected columns
     filtered_df = df[billed_columns + expected_columns]
     filtered_df = filtered_df[(filtered_df.index >= start_date_dt) & (filtered_df.index <= end_date_dt)]
