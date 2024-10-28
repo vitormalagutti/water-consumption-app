98878e704b9e77dc606f8bdb35adcd91b4014e40
vitormalagutti <vitoramalagutti@gmail.com>
2024-10-28 17:01:03 +0100

    Update water_app.py

--- water_app.py
+++ water_app.py
@@ -897,9 +897,6 @@ with tab1:
 
                 # Step 2: Join the correlation file with the original buildings file on 'Block Number'
                 merged_df = pd.merge(buildings_df, correlation_df, on='Block Number', how='left')
-                
-                total_volume = volume_df['04/23'].sum()
-                st.write("Total Volume1:", total_volume)
 
                 # Step 3: Merge volume_df and value_df on 'Subscription Number'
                 volume_df = pd.merge(correlation_df, volume_df, on='Subscription Number', how='left')
@@ -908,19 +905,12 @@ with tab1:
                 # Step 4: Group by 'Block Number' and sum Volume and Value
                 volume_summed = volume_df.groupby('Block Number').sum(numeric_only=True).reset_index()
                 value_summed = value_df.groupby('Block Number').sum(numeric_only=True).reset_index()
-                
-                total_volume = volume_df['04/23'].sum()
-                st.write("Total Volume2:", total_volume)
-                total_volume = volume_summed['04/23'].sum()
-                st.write("Total Volume3:", total_volume)
 
                 # Step 5: Merge the summed volumes and values back to the original df
                 billed_df = pd.merge(merged_df, volume_summed, on='Block Number', how='left')
                 billed_df = pd.merge(billed_df, value_summed, on='Block Number', how='left', suffixes=('_volume', '_value'))
-                # billed_df = billed_df.drop_duplicates(subset='Block Number')
-                billed_df
-                total_volume = billed_df['04/23_volume'].sum()
-                st.write("Total Volume4:", total_volume)
+                billed_df = billed_df.drop_duplicates(subset='Block Number')
+                
                 # Drop unnecessary columns
                 columns_to_drop = ['Population', 'Status', 'Subscription Number_x', 'Subscription Number_y', "Subscription Number"]
                 billed_df = billed_df.drop(columns=columns_to_drop, errors='ignore')
